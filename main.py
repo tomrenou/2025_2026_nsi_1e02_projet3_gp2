557cbc0d2f86a7d9dced6a49cfc49153073394a1
Tom <tom.renou2009@icloud.com>
2026-03-23 13:07:35 +0100

    wip

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Lior's Adventures")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
