67050bd67a465481f2e49369ade132b24bb94dd5
Tom <tom.renou2009@icloud.com>
2026-05-27 19:50:31 +0200

    w

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from levels import Level1
 
 # Initialisation
 pygame.init()
-
 WIDTH, HEIGHT = 800, 600
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Lior's Adventures")
-
 clock = pygame.time.Clock()
-
 # Couleurs
 WHITE = (255, 255, 255)
 BLACK = (20, 20, 30)
-
 # Thèmes
 themes = {
     "Bleu": {
@@ -38,19 +33,16 @@ themes = {
         "title": (80, 255, 80)
     }
 }
-
 current_theme = themes["Bleu"]
 
 # Polices
 title_font = pygame.font.SysFont("comicsansms", 80, bold=True)
 button_font = pygame.font.SysFont("comicsansms", 40, bold=True)
-
 char_font = pygame.font.SysFont(
     "comicsansms",
     31,
     bold=True
 )
-
 small_back_font = pygame.font.SysFont(
     "comicsansms",
     28,
@@ -67,32 +59,27 @@ character_colors = {
 
 # Images des persos
 character_images = {
-
     "Lior":
         pygame.transform.scale(
             pygame.image.load("Liorbleu.png").convert_alpha(),
             (200, 200)
         ),
-
     "Lior de glace":
         pygame.transform.scale(
             pygame.image.load("Lior_glace.png").convert_alpha(),
             (200, 200)
         ),
-
     "Lior ténébreux":
         pygame.transform.scale(
             pygame.image.load("Lior_tenebreux.png").convert_alpha(),
             (200, 200)
         ),
-
     "Lior de feu":
         pygame.transform.scale(
             pygame.image.load("Lior_feu.png").convert_alpha(),
             (200, 200)
         ),
 }
-
 # Chemins des images
 character_paths = {
     "Lior": "Liorbleu.png",
@@ -111,21 +98,18 @@ play_button = pygame.Rect(
     button_width,
     button_height
 )
-
 settings_button = pygame.Rect(
     (WIDTH - button_width) // 2,
     350,
     button_width,
     button_height
 )
-
 quit_button = pygame.Rect(
     (WIDTH - button_width) // 2,
     450,
     button_width,
     button_height
 )
-
 # Boutons paramètres
 colors_button = pygame.Rect(
     (WIDTH - button_width) // 2,
@@ -133,28 +117,24 @@ colors_button = pygame.Rect(
     button_width,
     button_height
 )
-
 character_button = pygame.Rect(
     (WIDTH - button_width) // 2,
     300,
     button_width,
     button_height
 )
-
 fullscreen_button = pygame.Rect(
     (WIDTH - button_width) // 2,
     400,
     button_width,
     button_height
 )
-
 back_button = pygame.Rect(
     (WIDTH - button_width) // 2,
     500,
     button_width,
     button_height
 )
-
 # Retour perso
 back_button_char = pygame.Rect(
     (WIDTH - 200) // 2,
@@ -162,28 +142,20 @@ back_button_char = pygame.Rect(
     200,
     50
 )
-
 # Sous menu couleurs
 theme_buttons = []
-
 y = 200
-
 for theme_name in themes:
-
     rect = pygame.Rect(
         (WIDTH - button_width) // 2,
         y,
         button_width,
         button_height
     )
-
     theme_buttons.append((rect, theme_name))
-
     y += 100
-
 # Choix personnages
 character_choices = [
-
     ("Lior",
      pygame.Rect(50, 200, 250, 60)),
 
@@ -196,7 +168,6 @@ character_choices = [
     ("Lior de feu",
      pygame.Rect(50, 440, 250, 60)),
 ]
-
 # Aperçu personnage
 preview_rect = pygame.Rect(
     370,
@@ -204,7 +175,6 @@ preview_rect = pygame.Rect(
     390,
     340
 )
-
 # États
 running = True
 
@@ -215,12 +185,8 @@ in_character_menu = False
 
 # Variables
 fullscreen = False
-
 selected_character = "Lior"
-
 level = None
-
-
 def draw_button(
     rect,
     text,
@@ -229,10 +195,8 @@ def draw_button(
     hover_color,
     font=None
 ):
-
     if font is None:
         font = button_font
-
     if rect.collidepoint(mouse_pos):
 
         pygame.draw.rect(
@@ -241,7 +205,6 @@ def draw_button(
             rect,
             border_radius=12
         )
-
     else:
 
         pygame.draw.rect(
@@ -250,9 +213,7 @@ def draw_button(
             rect,
             border_radius=12
         )
-
     label = font.render(text, True, WHITE)
-
     screen.blit(
         label,
         (
@@ -260,8 +221,6 @@ def draw_button(
             rect.y + (rect.height - label.get_height()) // 2
         )
     )
-
-
 def draw_rounded_rect_outline(
     surface,
     color,
@@ -269,7 +228,6 @@ def draw_rounded_rect_outline(
     radius,
     width=3
 ):
-
     pygame.draw.rect(
         surface,
         color,
@@ -278,90 +236,58 @@ def draw_rounded_rect_outline(
         border_radius=radius
     )
 
-
 while running:
-
     mouse_pos = pygame.mouse.get_pos()
-
     screen.fill(current_theme["bg"])
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             running = False
-
         if event.type == pygame.MOUSEBUTTONDOWN:
-
             # MENU PRINCIPAL
             if menu:
-
                 if play_button.collidepoint(event.pos):
-
                     menu = False
-
                     level = Level1(
                         screen,
                         character_paths[selected_character]
                     )
-
                 if settings_button.collidepoint(event.pos):
-
                     in_settings = True
                     menu = False
-
                 if quit_button.collidepoint(event.pos):
 
                     running = False
-
             # PARAMÈTRES
             elif in_settings:
-
                 if colors_button.collidepoint(event.pos):
 
                     in_settings = False
                     in_color_menu = True
-
                 if character_button.collidepoint(event.pos):
-
                     in_settings = False
                     in_character_menu = True
-
                 if fullscreen_button.collidepoint(event.pos):
-
                     fullscreen = not fullscreen
-
                     if fullscreen:
-
                         screen = pygame.display.set_mode(
                             (WIDTH, HEIGHT),
                             pygame.FULLSCREEN
                         )
-
                     else:
-
                         screen = pygame.display.set_mode(
                             (WIDTH, HEIGHT)
                         )
-
                 if back_button.collidepoint(event.pos):
-
                     menu = True
                     in_settings = False
-
             # MENU COULEURS
             elif in_color_menu:
-
                 for rect, theme_name in theme_buttons:
-
                     if rect.collidepoint(event.pos):
-
                         current_theme = themes[theme_name]
-
                 if back_button.collidepoint(event.pos):
-
                     in_color_menu = False
                     in_settings = True
-
             # MENU PERSONNAGES
             elif in_character_menu:
 
@@ -468,13 +394,11 @@ while running:
 
     # MENU COULEURS
     elif in_color_menu:
-
         settings_text = title_font.render(
             "Couleurs",
             True,
             current_theme["title"]
         )
-
         screen.blit(
             settings_text,
             (
@@ -482,9 +406,7 @@ while running:
                 50
             )
         )
-
         for rect, theme_name in theme_buttons:
-
             draw_button(
                 rect,
                 theme_name,
@@ -492,7 +414,6 @@ while running:
                 current_theme["btn_base"],
                 current_theme["btn_hover"]
             )
-
         draw_button(
             back_button,
             "Retour",
@@ -503,13 +424,11 @@ while running:
 
     # MENU PERSONNAGES
     elif in_character_menu:
-
         settings_text = title_font.render(
             "Personnages",
             True,
             current_theme["title"]
         )
-
         screen.blit(
             settings_text,
             (
@@ -517,10 +436,8 @@ while running:
                 50
             )
         )
-
         # Boutons
         for name, rect in character_choices:
-
             draw_button(
                 rect,
                 name,
@@ -529,7 +446,6 @@ while running:
                 current_theme["btn_hover"],
                 font=char_font
             )
-
         # Cadre
         draw_rounded_rect_outline(
             screen,
@@ -537,22 +453,17 @@ while running:
             preview_rect,
             radius=18
         )
-
         # Image perso
         img = character_images[selected_character]
-
         img_x = (
             preview_rect.x
             + (preview_rect.width - img.get_width()) // 2
         )
-
         img_y = (
             preview_rect.y
             + (preview_rect.height - img.get_height()) // 2
         )
-
         screen.blit(img, (img_x, img_y))
-
         draw_button(
             back_button_char,
             "Retour",
@@ -561,15 +472,10 @@ while running:
             current_theme["btn_hover"],
             font=small_back_font
         )
-
     # JEU
     else:
-
         level.update()
         level.draw()
-
     pygame.display.flip()
-
     clock.tick(60)
-
 pygame.quit()
